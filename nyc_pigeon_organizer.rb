c3193fc991562bca00e18f3117d3c41b33ef56de
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-19 22:37:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,6 @@
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array| 
-    
+    value.each do |inner_key, name>| 
+      names.each do |name|
+        final_array
   end
